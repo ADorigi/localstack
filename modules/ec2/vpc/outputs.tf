98223debe6118d95aa3a4fb85e0ffd8c2d445744
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2024-08-14 17:09:36 -0400

    fix: vpc output

--- modules/ec2/vpc/outputs.tf
+++ modules/ec2/vpc/outputs.tf
@@ -1,8 +1,4 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.project[*].id
-}
-
-output "igw_id" {
-  value = aws_internet_gateway.igw[*].id
+  value       = aws_vpc.project.id
 }
